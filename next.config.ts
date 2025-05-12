76179820829f54b0536ef82e45ce747599753c77
QuiLion7 <quilbrub@gmail.com>
2025-05-12 08:35:24 -0300

    chore: update settings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@tabler/icons-react", "motion"],
-  swcMinify: true,
   experimental: {
     forceSwcTransforms: true,
   },
